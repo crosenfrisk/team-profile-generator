0c95c7baaaf1ad8310ff657344062dd9728b6261
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-29 15:14:54 -0600

    Adding Employee.js Engineer.js Intern.js and Manager.js to lib directory; wrote functions getName(), getId(), getEmail(), and getRole() inside index.js but might move to Employee.js to reference/pull from. Using inquirer prompts to get input from user

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(), err => {
-    if (err) throw err;
+class Employee {
+  constructor(name, role){
+    this.name = getName;
+    this.role = getRole;
+  }
+}
+
+const getName = () =>{
+    return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+}
+
+const getId = () =>{
+  return inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your employee id?'
+  }
+]);
+}
+
+const getEmail = () =>{
+  return inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your employee email address?'
+  }
+]);
+}
+
+const getRole = () =>{
+  return inquirer.prompt([
+    {
+      type: 'checkbox',
+      choices: ['Manager', 'Engineer', 'Intern'],
+      message: 'What is your role?'
+    }
+  ]);
+}
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+
+//   ]);
+// };
+
+// promptUser().then(answers => console.log(answers));
+
+
+// fs.writeFile('index.html', generatePage(), err => {
+//     if (err) throw err;
   
-    console.log('Team Webpage complete! Check out index.html to see the output!');
-  });
+//     console.log('Team Webpage complete! Check out index.html to see the output!');
+//   });
