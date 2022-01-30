02eddcaaae3a3a3ea2bc987bde1572ac460b017b
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-29 20:23:33 -0600

    added back commented out userPrompt in index.js for test; not working yet

--- index.js
+++ index.js
@@ -1,23 +1,75 @@
 const inquirer = require('inquirer');
-const Employee = require('.lib/Employee');
-const Engineer = require('.lib/Engineer');
-const Manager = require('.lib/Manager');
-const Intern = require('.lib/Intern');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 
-// const promptUser = () => {
-//   return inquirer.prompt([
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username:'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Provide your email address:'
+      }
+  ]);
+}
 
-//   ]);
-// };
+const promptProject = () => {
+    console.log(`
+  =======================
+   Add a New Team Member
+  =======================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your team member? (Required)'
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is the employee id for your team member?'
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: 'What is the role of your employee?',
+        choices: ['Manager', 'Engineer', 'Intern']
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the email address of your employee.'
+        },
+      {
+        type: 'confirm',
+        name: 'confirmAddTeamMember',
+        message: 'Would you like to add another team member?',
+        default: false
+      }
+    ]);
+  };
 
-// promptUser().then(answers => console.log(answers));
+promptUser()
+.then(answers => console.log(answers));
 
 
-// fs.writeFile('index.html', generatePage(), err => {
-//     if (err) throw err;
+fs.writeFile('.dist/index.html', generatePage(), err => {
+    if (err) throw err;
   
-//     console.log('Team Webpage complete! Check out index.html to see the output!');
-//   });
+    console.log('Team Webpage complete! Check out index.html to see the output!');
+  });
