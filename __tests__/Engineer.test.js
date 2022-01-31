5d50cbb46977c675c6a21609f67b373f4411b083
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-31 13:30:20 -0600

    modified Engineer.test.js first test to have property github in addition to testing getGithub() function

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,17 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
 const Employee = require('../lib/Employee.js');
+const { expect } = require('@jest/globals');
 
-test('Using Employee properties, Engineer should have name, id, and email', () => {
+test('Using Employee properties, Engineer should have name, id, email, and github username', () => {
   const engineer = new Engineer('Harold');
 
   expect(engineer).toHaveProperty('name');
   expect(engineer).toHaveProperty('id');
   expect(engineer).toHaveProperty('email');
+  expect(engineer).toHaveProperty('github');
 
 })
 
 test('looks for GitHub username property using getGithub() function', () => {
-  const engineer = new Engineer('Harold', 1, 'test@email.com', 'githubUsername');
+  const engineer = new Engineer('Harold', 1, 'test@email.com', 'github');
 
   // Check for name input, being a string and length greater than zero.
 
