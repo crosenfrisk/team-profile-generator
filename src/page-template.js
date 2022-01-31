7fa4999fbcf8629824f85731c935a42230d0a58b
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-01-31 14:15:15 -0600

    made changes to develop branch, specifically index.html template

--- src/page-template.js
+++ src/page-template.js
@@ -8,11 +8,16 @@ const generatePage = () => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Team Profile Generator</title>
+      <title>My Team Profile</title>
     </head>
   
     <body>
-      <h1></h1>
+      <header>
+        <h1>My Team</h1>
+      </header>
+
+      <main>
+
       <h2><a href="https://github.com/...">Github</a></h2>
     </body>
     </html>
